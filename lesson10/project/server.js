92536582114daef0209632a3ad6e6bdfa8bf3b29
junhao-zhang7 <3117207985@qq.com>
2026-03-01 20:03:21 +0800

    新课

--- lesson10/project/server.js
+++ lesson10/project/server.js
@@ -1,14 +1,16 @@
-const express=require("express")
-const app=express();
-const PORT=3000;
-app.use(express.static("public"))
-app.post("/login",(request,response)=>{
-    console.log(request.body)
-    response.json({
-        ok:true,
-        received:request.body,
-    })
-})
-app.listen(PORT,()=>{
-    console.log("当前服务运行在3000端口号")
-})
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.post("/login", (request, response) => {
+  console.log(request.body);
+  response.json({
+    ok: true,
+    received: request.body,
+  });
+});
+app.listen(PORT, () => {
+  console.log("当前服务运行在3000端口号");
+});
